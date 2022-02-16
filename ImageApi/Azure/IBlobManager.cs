322f27e2abeea037d7230c8b23fcdde48aa1b35b
Gabriel Rossier <rossier.gabriel218@gmail.com>
2022-02-16 09:54:02 +0100

    Base project

---
+++ ImageApi/Azure/IBlobManager.cs
@@ -0,0 +1,10 @@
+﻿namespace ImageApi.Azure
+{
+    public interface IBlobManager
+    {
+        Task CreateObject(string objectUrl, string filePath = "");
+        Task<Boolean> ObjectExists(string objectUrl);
+        Task RemoveObject(string objectUrl);
+        Task DownloadObject(string objectUrl, string destinationUri);
+    }
+}
---
+++ TestImageApi/Tests/testFile.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+
+namespace TestImageApi.Tests
+{
+    public class testFile
+    {
+        [SetUp]
+        public void Init()
+        {
+        }
+
+        [Test]
+        public void testUnit()
+        {
+
+            //then
+            Assert.IsTrue(true);
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+
+        }
+    }
+}
