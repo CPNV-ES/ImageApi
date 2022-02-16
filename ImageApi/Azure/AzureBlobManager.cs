4d35a3ae1cd0d8ab0d9cf9915215f71e3ed5cb17
Gabriel Rossier <rossier.gabriel218@gmail.com>
2022-02-16 09:54:02 +0100

    Base project

---
+++ ImageApi/Azure/AzureBlobManager.cs
@@ -0,0 +1,31 @@
+﻿namespace ImageApi.Azure
+{
+    public class AzureBlobManager : IBlobManager
+    {
+
+        public AzureBlobManager(string bucketUrl)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task CreateObject(string objectUrl, string filePath = "")
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DownloadObject(string objectUrl, string destinationUri)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> ObjectExists(string objectUrl)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveObject(string objectUrl)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
