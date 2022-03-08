baba2c38f798bc16796b648ffcc075932ac8005a
Schouwey <zazar981@hotmail.com>
2022-03-08 10:08:38 +0100

    Add Image Controller

---
+++ ImageApi/Controllers/ImageController.cs
@@ -0,0 +1,64 @@
+﻿using ImageApi.Azure;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ImageApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImageController : ControllerBase
+    {
+        private IWebHostEnvironment _hostingEnvironment;
+        private AzureBlobManager _blobManager;
+        public ImageController(IWebHostEnvironment environment)
+        {
+            _hostingEnvironment = environment;
+            _blobManager = new AzureBlobManager(Environment.GetEnvironmentVariable("AZURE_STORAGE_CONNECTIONSTRING"));
+        }
+        // GET: api/<ImageController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<ImageController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ImageController>
+        [HttpPost]
+        public async Task Post(IFormFile file)
+        {
+            string uploads = Path.Combine(_hostingEnvironment.ContentRootPath, "uploads");
+            string filePath = "";
+            if (file.Length > 0)
+            {
+                filePath = Path.Combine(uploads, file.FileName);
+                using (Stream fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    await file.CopyToAsync(fileStream);
+                }
+            }
+            Console.WriteLine("ImageAnalysis/" + file.FileName, filePath);
+            await this._blobManager.CreateObject("ImageAnalysis/"+ file.FileName, filePath);
+            Console.WriteLine("Finished");
+        }
+
+        // PUT api/<ImageController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ImageController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
