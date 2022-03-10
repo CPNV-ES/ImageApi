a220468375d258a7d902627c021c18176fcfd988
Dimitri Imfeld <dimfeld@protonmail.ch>
2022-03-10 12:22:09 +0100

    Fix merge

--- ImageApi/Controllers/ImageController.cs
+++ ImageApi/Controllers/ImageController.cs
@@ -23,7 +23,8 @@ namespace ImageApi.Controllers
         public ImageController(IWebHostEnvironment environment)
         {
             _hostingEnvironment = environment;
-         
+            _blobManager = new AzureBlobManager(connectionString);
+            _cvManager = new CvManager(cognitiveEndPoint, cognitiveKey);
         }
         
 
